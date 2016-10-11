5b9ca95f2617ca3fe5ec1bf18f34f04e51b0afaf
stephybee <stephybee13@gmail.com>
2016-10-10 21:35:08 -0700

    update datafile.hpp

--- datafile.hpp
+++ datafile.hpp
@@ -1,4 +1,3 @@
-
 // datafile.hpp
 // CSUF CPSC 131, Fall 2016, Project 1
 //
@@ -16,7 +15,7 @@
 // Helper to throw std::runtime_error due to a parse error in the data
 // file.
 void throw_decode_exception() {
-  throw std::runtime_error("product database decode error");
+	throw std::runtime_error("product database decode error");
 }
 
 // Load products from a .dat file with the given filename.
@@ -28,52 +27,53 @@ void throw_decode_exception() {
 // Throws std::runtime_error if the file cannot be opened, or is not a
 // properly formatted .dat file.
 Catalog* load_products(const std::string& filename) {
-  std::ifstream f(filename.c_str());
-  if (!f)
-    throw std::runtime_error("cannot open product file");
+	std::ifstream f(filename.c_str());
+	if (!f)
+		throw std::runtime_error("cannot open product file");
 
-  // used for individual tokens that we skip over
-  std::string s;
+	// used for individual tokens that we skip over
+	std::string s;
+
+	getline(f, s);
+	if (!f || (s != "PRODUCTS DATABASE"))
+		throw_decode_exception();
+
+	int count;
+	f >> s >> count;
+	if (!f || (s != "COUNT") || (count < 1))
+		throw_decode_exception();
+	getline(f, s); // newline
+	getline(f, s); // blank line
+
+	Catalog* c = new Catalog(count);
+
+	for (int i = 0; i < count; ++i) {
+		std::string code, name;
+		int price_cents;
+		f >> s // UPC or PLU
+			>> s // CODE
+			>> code; // code string
+		getline(f, s); // newline
+		getline(f, name); // name on its own line
+		f >> s // PRICE
+			>> s // BY
+			>> s // COUNT or POUND
+			>> price_cents; // price in cents
+		getline(f, s); // newline
+		getline(f, s); // blank line
+
+		double price_dollars = static_cast<double>(price_cents) / 100.0;
+
+		c->addProduct(code, name, price_dollars);
+	}
+
+	if (!f) {
+		delete c;
+		throw_decode_exception();
+	}
+
+	f.close();
   
-  getline(f, s);
-  if (!f || (s != "PRODUCTS DATABASE"))
-    throw_decode_exception();
-
-  int count;
-  f >> s >> count;
-  if (!f || (s != "COUNT") || (count < 1))
-    throw_decode_exception();
-  getline(f, s); // newline
-  getline(f, s); // blank line
-
-  Catalog* c = new Catalog(count);
-  
-  for (int i = 0; i < count; ++i) {
-    std::string code, name;
-    int price_cents;
-    f >> s // UPC or PLU
-      >> s // CODE
-      >> code; // code string
-    getline(f, s); // newline
-    getline(f, name); // name on its own line
-    f >> s // PRICE
-      >> s // BY
-      >> s // COUNT or POUND
-      >> price_cents; // price in cents
-    getline(f, s); // newline
-    getline(f, s); // blank line
-
-    double price_dollars = static_cast<double>(price_cents) / 100.0;
-    
-    c->addProduct(code, name, price_dollars);
-  }
-
-  if (!f) {
-    delete c;
-    throw_decode_exception();
-  }
-
-  f.close();
-
-  return c;
+  system("pause");
+	return c;
 }
